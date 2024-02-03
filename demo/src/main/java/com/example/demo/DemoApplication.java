e532a8c5ae8070c19c520f9296aec53d6f7ef3be
unknown <your.email@example.com>
2024-02-03 17:10:48 +0530

    thi com

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Hi ra ... you have done your first work");
 		SpringApplication.run(DemoApplication.class, args);
+		
+		System.out.println("Hi ra ... you have done your first work");
 	}
 
 }
